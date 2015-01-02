7f737d5626f43dcab4ec442971cfd79bea610970
josemrb <work@josemrb.com>
2015-01-02 15:57:26 -0400

    update dotfiles installer/uninstaller

--- bin/dotfiles-install.zsh
+++ bin/dotfiles-install.zsh
@@ -4,19 +4,19 @@ setopt EXTENDED_GLOB
 pwd=${0:a:h}
 
 # prezto
-for rcfile in $pwd/prezto/*(.N); do
+for rcfile in $pwd/../prezto/*(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 # tmux
-for rcfile in $pwd/tmux/*(.N); do
+for rcfile in $pwd/../tmux/*(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
 # vim
-for rcfile in $pwd/vim/*(.N); do
+for rcfile in $pwd/../vim/*(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
@@ -24,12 +24,13 @@ git clone https://github.com/gmarik/Vundle.vim.git "${ZDOTDIR:-$HOME}/.vim/bundl
 vim +PluginInstall +qall
 
 # git
-for rcfile in $pwd/git/*(.N); do
+for rcfile in $pwd/../git/*(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
 # ssh
-for rcfile in $pwd/ssh/*(.N); do
+mkdir -p "${ZDOTDIR:-$HOME}/.ssh
+for rcfile in $pwd/../ssh/*(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.ssh/${rcfile:t}"
 done
 
