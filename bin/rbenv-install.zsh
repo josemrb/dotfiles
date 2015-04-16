b87feec7d9e59fdf42fa47ff8aa77ab5f528fc7d
josemrb <work@josemrb.com>
2015-04-15 23:48:17 -0400

    update in progress

--- bin/rbenv-install.zsh
+++ bin/rbenv-install.zsh
@@ -14,9 +14,15 @@ local rb
 rb=$(git describe --tags | cut --delimiter="-" -f1)
 git checkout $rb
 
+git clone https://github.com/josemrb/rbenv-ripper-tags.git $HOME/.rbenv/plugins/rbenv-ripper-tags
+cd $HOME/.rbenv/plugins/ruby-build
+git fetch --tags
+local rb
+rb=$(git describe --tags | cut --delimiter="-" -f1)
+git checkout $rb
 cat <<EOF > $HOME/.rbenv/default-gems
-bundler
 gem-ripper-tags
+bundler
 gem-browse
 EOF
 
